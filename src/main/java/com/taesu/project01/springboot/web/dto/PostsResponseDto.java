e16a9f237804f7f7402e06b7016a05387fac0e9b
TaeSuPark <ptss0129@gmail.com>
2021-01-13 22:04:16 +0900

    add two Dto(PostsResponseDto, PostsUpdateRequestDto) for Update

---
+++ src/main/java/com/taesu/project01/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.taesu.project01.springboot.web.dto;
+
+import com.taesu.project01.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
