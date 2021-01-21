b49a8dd9719e0a79fd0c1fd48ad198757c203fd2
TaeSuPark <xotn1358@naver.com>
2021-01-21 15:47:23 +0900

    add mustache layout and modified IndexController

--- src/main/java/com/taesu/project01/springboot/web/IndexController.java
+++ src/main/java/com/taesu/project01/springboot/web/IndexController.java
@@ -1,14 +1,22 @@
 package com.taesu.project01.springboot.web;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//페이지 이동 관련 컨트롤러로 사용
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
