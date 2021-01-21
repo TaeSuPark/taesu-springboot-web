3f386fe2f1aca49a43c1f4f6fa9460d9f41a3972
TaeSuPark <xotn1358@naver.com>
2021-01-21 15:30:01 +0900

    add IndexControllerTest

---
+++ src/test/java/com/taesu/project01/springboot/web/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package com.taesu.project01.springboot.web;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void load_index() {
+
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("Start Page in Spring boot with Mustache");
+    }
+
+}
