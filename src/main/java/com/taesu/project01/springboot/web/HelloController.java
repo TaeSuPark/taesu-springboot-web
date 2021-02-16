5f6d8a1ac560fbd247d3b060130d19f3fe757d3e
TaeSuPark <ptss0129@gmail.com>
2021-02-16 15:06:22 +0900

    add some comments

--- src/main/java/com/taesu/project01/springboot/web/HelloController.java
+++ src/main/java/com/taesu/project01/springboot/web/HelloController.java
@@ -5,7 +5,7 @@ import com.taesu.project01.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//Posts entity와 통신하지 않는 컨트롤러
 @RestController // 구 @ResponseBody
 public class HelloController {
 
